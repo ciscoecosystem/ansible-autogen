add7d8d1c1c0690f38c941ddb8a153b631db8676
gramnani-crest <girish.ramnani@crestdatasys.com>
2018-08-08 18:21:11 +0530

    Added service template

--- utils.py
+++ utils.py
@@ -1,11 +1,17 @@
 from jinja2 import Environment, FileSystemLoader
-
+import re
 
 # this is similar to capitalize, just that this doesnt lower case everything else
 def capital(value):
     return value[0].upper() + value[1:]
 
+def snakify(name):
+    s1 = re.sub('(.)([A-Z][a-z]+)', r'\1_\2', name)
+    return re.sub('([a-z0-9])([A-Z])', r'\1_\2', s1).lower()
+
+
 def render(template_name, context):
     env = Environment(loader=FileSystemLoader('.'))
     env.filters["capital"] = capital
+    env.filters["snakify"] = snakify
     return env.get_template(template_name).render(context)
