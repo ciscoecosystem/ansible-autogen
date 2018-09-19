fb01278c6e0037f86dec394624afb5e7f0b83a62
girishramnani <girishramnani95@gmail.com>
2018-09-19 11:30:13 +0530

    added relationship

--- metagen.py
+++ metagen.py
@@ -31,54 +31,72 @@ def generateClassMeta(className):
     paciClassMeta['isConfigurable'] = classMeta.isConfigurable()
     paciClassMeta['isDeletable'] = classMeta.isDeletable()
     paciClassMeta['help'] = classMeta.getHelp()
-    paciClassMeta['name'] = classMeta.getPkgName() + ":" + classMeta.getClassName()
-
+    #paciClassMeta['name'] = classMeta.getPkgName() + ":" + classMeta.getClassName()
+    paciClassMeta['isRelation'] = classMeta.isRelation()
     paciClassMeta['identifiedBy'] = (
         [x for x in classMeta.getOrderedNamingProps()]
     )
     paciClassMeta['rnFormat'] = re.sub(r'(%\((\w+)\)s)',
                                        r'{\2}',
                                        classMeta.getRnFormat())
-    paciClassMeta['properties'] = {
-        p.getName(): {
-            'isConfigurable': p.isConfig(),
-            'help': p._getHelp(),
-            # 'options': p.getType().getLabelConstMap()
-            # use p.getType().getConstants() for both str label and internal value
-            'options': p.getType()._constMap.keys() if p.isConfig() else []
-        }
-        for p in classMeta.getProperties()
-    }
+#    paciClassMeta['properties'] = {
+#        p.getName(): {
+#            'isConfigurable': p.isConfig(),
+#            'help': p._getHelp(),
+#            # 'options': p.getType().getLabelConstMap()
+#            # use p.getType().getConstants() for both str label and internal value
+#            'options': p.getType()._constMap.keys() if p.isConfig() else []
+#        }
+#        for p in classMeta.getProperties()
+#    }
+    paciClassMeta['contains'] = []
+    for x in classMeta.getContainedClasses():
+        paciClassMeta['contains'].append(getPaciClassName(x))
+    
+    paciClassMeta['containers'] = []
+    for x in classMeta.getContainerClasses():
+        paciClassMeta['containers'].append(getPaciClassName(x))
+    
 
-    paciClassMeta['contains'] = {
-        getPaciClassName(x): '' for x in classMeta.getContainedClasses()
-    }
-    paciClassMeta['containers'] = {
-        getPaciClassName(x): '' for x in classMeta.getContainerClasses()
-    }
+#    paciClassMeta['rnMap'] = {}
+#    for meta in classMeta.getContainedClasses():
+#        numNamingProps = 0
+#        for _ in meta.getOrderedNamingProps():
+#            numNamingProps += 1
+#        rnPrefix = getRnPrefix(meta)
+#        # ngen allows two contained classes to have the same RN prefix
+#        # as long as one has no naming property, and other has naming
+#        # property(ies).
+#        if numNamingProps > 0:
+#            rnPrefix += '-'
+#        t = getPaciClassName(meta)
+#        if rnPrefix in paciClassMeta['rnMap']:
+#            print('Warning: {} is a duplicate RN prefix for {} under {}'
+#                  ', other one is {}'.format(
+#                      rnPrefix, t, paciClassName,
+#                      paciClassMeta['rnMap'][rnPrefix]))
+#        else:
+#            paciClassMeta['rnMap'][rnPrefix] = t
 
-    paciClassMeta['rnMap'] = {}
+            
+    paciClassMeta["relationTo"] = []
+    paciClassMeta["relationFrom"] = []
+    #ONE_TO_ONE, ONE_TO_M, N_TO_ONE, N_TO_M = range(4)
     for meta in classMeta.getContainedClasses():
-        numNamingProps = 0
-        for _ in meta.getOrderedNamingProps():
-            numNamingProps += 1
-        rnPrefix = getRnPrefix(meta)
-        # ngen allows two contained classes to have the same RN prefix
-        # as long as one has no naming property, and other has naming
-        # property(ies).
-        if numNamingProps > 0:
-            rnPrefix += '-'
-        t = getPaciClassName(meta)
-        if rnPrefix in paciClassMeta['rnMap']:
-            print('Warning: {} is a duplicate RN prefix for {} under {}'
-                  ', other one is {}'.format(
-                      rnPrefix, t, paciClassName,
-                      paciClassMeta['rnMap'][rnPrefix]))
-        else:
-            paciClassMeta['rnMap'][rnPrefix] = t
+        ### Check if contained class is a relationship class
+        if meta.isRelation():
+            temppaciClassMeta = {}
+            ### is a source relation class
+            if hasattr(meta,'_type'):
+                temppaciClassMeta["cardinality"] = meta._type
+                temppaciClassMeta["class"] = meta.getTargetClassName().replace('.','')
+                paciClassMeta["relationTo"].append(temppaciClassMeta)
+            else: ### is target relation class 
+                temppaciClassMeta["cardinality"] = -1
+                temppaciClassMeta["class"] = meta.getSourceClassName().replace('.','')
+                paciClassMeta["relationFrom"].append(temppaciClassMeta)
 
     return (paciClassName, paciClassMeta)
-
 
 def main():
     classNames = dir.getClassNames()
